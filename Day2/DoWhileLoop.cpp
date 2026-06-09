766b1cb1b9833cb812c56f10ae17eb2601efda87
HARSH SHARMA <harshsharma@HARSHs-MacBook-Air-2.local>
2026-06-09 10:40:07 +0530

    first commit

---
+++ Day2/DoWhileLoop.cpp
@@ -0,0 +1,12 @@
+// print 1 to 100 using do while loop
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;
+    do {
+        cout << i << " ";
+        i++;
+    } while (i <= 100);
+    return 0;
+}
