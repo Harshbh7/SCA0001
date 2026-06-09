591dd799ff8897c88649b09a583064a24f0bb79b
HARSH SHARMA <harshsharma@HARSHs-MacBook-Air-2.local>
2026-06-09 10:36:30 +0530

    first commit

--- Day2/EvenNo.cpp
+++ Day2/EvenNo.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 using namespace std;
 
+// use bitwise operator to check if a number is even or odd
+
 int main() {
     for (int i = 1; i <= 100; i++) {
         if ((i & 1) == 0) { 
---
+++ Day2/whileLoop.cpp
@@ -0,0 +1,12 @@
+// print 1 to 100 using while loop
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i = 1;
+    while (i <= 100) {
+        cout << i << " ";
+        i++;
+    }
+    return 0;
+}
