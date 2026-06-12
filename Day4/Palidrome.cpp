99aa46f2505d8447c2384bfe0820ff307250f788
HARSH SHARMA <harshsharma@HARSHs-MacBook-Air-2.local>
2026-06-12 10:36:03 +0530

    Re-upload project folders

---
+++ Day4/Palidrome.cpp
@@ -0,0 +1,31 @@
+// check string is palindrome or not using two pointer approach
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    cout << "Enter a string: ";
+    cin >> str;
+
+    int left = 0;
+    int right = str.length() - 1;
+    bool isPalindrome = true;
+
+    while (left < right) {
+        if (str[left] != str[right]) {
+            isPalindrome = false;
+            break;
+        }
+        left++;
+        right--;
+    }
+
+    if (isPalindrome) {
+        cout << "The string is a palindrome." << endl;
+    } else {
+        cout << "The string is not a palindrome." << endl;
+    }
+
+    return 0;
+}
