ac0c72e6df7e61e7bad6bee8067d27aa860ad888
HARSH SHARMA <harshsharma@HARSHs-MacBook-Air-2.local>
2026-06-23 10:52:22 +0530

    SCAS001

--- Day9/MinHeap.cpp
+++ Day9/MinHeap.cpp
@@ -1,7 +1,86 @@
 // Min Heap 
 #include <iostream>
+#include <vector>
 using namespace std;
 
+class MinHeap {
+    vector<int> heap;
+
+    void heapifyUp(int index) {
+        while (index > 0) {
+            int parent = (index - 1) / 2;
+
+            if (heap[parent] > heap[index]) {
+                swap(heap[parent], heap[index]);
+                index = parent;
+            } else {
+                break;
+            }
+        }
+    }
+
+    void heapifyDown(int index) {
+        int n = heap.size();
+
+        while (true) {
+            int smallest = index;
+            int left = 2 * index + 1;
+            int right = 2 * index + 2;
+
+            if (left < n && heap[left] < heap[smallest])
+                smallest = left;
+
+            if (right < n && heap[right] < heap[smallest])
+                smallest = right;
+
+            if (smallest != index) {
+                swap(heap[index], heap[smallest]);
+                index = smallest;
+            } else {
+                break;
+            }
+        }
+    }
+
+public:
+    void insert(int value) {
+        heap.push_back(value);
+        heapifyUp(heap.size() - 1);
+    }
+
+    void deleteMin() {
+        if (heap.empty()) return;
+
+        heap[0] = heap.back();
+        heap.pop_back();
+        heapifyDown(0);
+    }
+
+    int getMin() {
+        return heap[0];
+    }
+
+    void display() {
+        for (int x : heap)
+            cout << x << " ";
+        cout << endl;
+    }
+};
+
 int main() {
-    
+    MinHeap h;
+
+    h.insert(10);
+    h.insert(5);
+    h.insert(20);
+    h.insert(2);
+
+    cout << "Min Element: " << h.getMin() << endl;
+
+    h.deleteMin();
+
+    cout << "After deletion: ";
+    h.display();
+
+    return 0;
 }
